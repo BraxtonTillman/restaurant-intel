fff8faf765fdcc9fe40501c0d49f83e8c228e8d2
BraxtonTillman <ColeTillman21@gmail.com>
2026-02-03 13:36:54 -0500

    feat: add sales_orders table migration and update Docker configuration

---
+++ backend/app/db/migrations/versions/c54fe3241012_add_sales_orders_table.py
@@ -0,0 +1,32 @@
+"""Add sales_orders table
+
+Revision ID: c54fe3241012
+Revises: 04723d25ab9f
+Create Date: 2026-02-03 13:04:57.728100
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = 'c54fe3241012'
+down_revision: Union[str, Sequence[str], None] = '04723d25ab9f'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.create_table(
+        'sales_orders',
+        sa.Column('id', sa.Integer(), primary_key=True),
+        sa.Column('ingestion_run_id', sa.Integer(), sa.ForeignKey('ingestion_runs.id'), nullable=False),
+        sa.Column('order_id', sa.String(length=100), nullable=False),
+        sa.Column('occurred_at', sa.DateTime(), server_default=sa.func.now(), nullable= False),
+        sa.Column('total', sa.Float()),
+        sa.Column('created_at', sa.DateTime(), server_default=sa.func.now(), nullable=False),
+    )
+
+def downgrade():
+    op.drop_table('sales_orders')
--- scratch.sql
+++ scratch.sql
@@ -2,3 +2,5 @@ SELECT *
 FROM alembic_version;
 SELECT *
 FROM ingestion_runs;
+SELECT *
+FROM sales_orders;
